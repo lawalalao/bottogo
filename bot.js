ac8c561f374640008101e05034ca33b3cffed7d4
litorod <litorod@users.github.com>
2020-08-17 14:21:48 +0100

    Add code

---
+++ bot.js
@@ -0,0 +1,53 @@
+const config = require("./config");
+const twit = require("twit");
+
+const T = new twit(config);
+
+function retweet(searchText) {
+  // Params to be passed to the 'search/tweets' API endpoint
+  let params = {
+    q: searchText + "",
+    result_type: "mixed",
+    count: 25,
+  };
+
+  T.get("search/tweets", params, function (
+    err_search,
+    data_search,
+    response_search
+  ) {
+    let tweets = data_search.statuses;
+    if (!err_search) {
+      let tweetIDList = [];
+      for (let tweet of tweets) {
+        tweetIDList.push(tweet.id_str);
+
+        //more code here later...
+      }
+
+      // Call the 'statuses/retweet/:id' API endpoint for retweeting EACH of the tweetID
+      for (let tweetID of tweetIDList) {
+        T.post("statuses/retweet/:id", { id: tweetID }, function (
+          err_rt,
+          data_rt,
+          response_rt
+        ) {
+          if (!err_rt) {
+            console.log("\n\nRetweeted! ID - " + tweetID);
+          } else {
+            console.log("\nError... Duplication maybe... " + tweetID);
+            console.log("Error = " + err_rt);
+          }
+        });
+      }
+    } else {
+      console.log("Error while searching" + err_search);
+      process.exit(1);
+    }
+  });
+}
+
+// Run every 60 seconds
+setInterval(function () {
+  retweet("#TT228 OR #Tgtwittos");
+}, 60000);
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: "vj2NKCzDX2t6nzqmVh9t3J2SX",
+  consumer_secret: "f8XkUBc3b5qfiIVmq3jNpyL2oT5CpsWa3adEtH0gLI9rtA78gw",
+  access_token: "1292015343196667904-AUWP8enbHjIBSktkgxsIvCXBN9cfnB",
+  access_token_secret: "U0PGltUUHGQH1nzqag1HPuKvjDJg6fnzueTp4zorHvItP",
+};
